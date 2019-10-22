400ef41558af19124ad45e8a8e099da6520a9e69
Arturas <bortkevicius.arturas@gmail.com>
2019-10-22 18:44:03 +0300

    Lukui

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './assets/style.scss';
+import './assets/img/flag.png';
+import './assets/img/pencil.png';
+import './assets/img/gear.png';
+import './assets/img/rocket.png';
+import './assets/img/1.png';
+import './assets/img/2.png';
+import './assets/img/3.png';
+import './assets/img/4.png';
